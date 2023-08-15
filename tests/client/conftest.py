b5886ddfd36164f023f10eed56fcffae177c71f4
Angela Dang Atkinson <Angela.DangAtkinson@ovintiv.com>
2023-08-15 14:52:55 -0600

    Add tests

---
+++ tests/client/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from whitson.client.config import Token
+
+
+@pytest.fixture()
+def token():
+    token = Token(
+        access_token="abcdefghijklmnopqrstuvwxyz",
+        scope="get:api post:api delete:api",
+        issued_at=4102444800.0,  # Friday, January 1, 2100 12:00:00 AM
+        expires_in=86400,  # 24 hours
+        token_type="Bearer"
+    )
+    yield token

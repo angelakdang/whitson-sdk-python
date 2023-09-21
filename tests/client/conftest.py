4a0cb6c74585e88105b9fe82bca97138261365ce
Angela Dang Atkinson <Angela.DangAtkinson@ovintiv.com>
2023-09-21 14:17:04 -0600

    Update tests

--- tests/client/conftest.py
+++ tests/client/conftest.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 from whitson.client.config import Token
@@ -13,3 +15,15 @@ def token():
         token_type="Bearer",
     )
     yield token
+
+
+@pytest.fixture()
+def token_path():
+    token = {
+        "access_token": "abcdefghijklmnopqrstuvwxyz",
+        "scope": "get:api post:api delete:api",
+        "issued_at": 4102444800.0,  # Friday, January 1, 2100 12:00:00 AM
+        "expires_in": 86400,  # 24 hours
+        "token_type": "Bearer",
+    }
+    yield json.dumps(token, indent=4)
